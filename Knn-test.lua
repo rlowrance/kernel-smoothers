ba61f5c8fb147f089470f5a94d9b84c963c941cb
Roy Lowrance <roy.lowrance@gmail.com>
2012-08-10 07:58:43 -0400

    catch up

--- Knn-test.lua
+++ Knn-test.lua
@@ -28,17 +28,21 @@ function tests.testEstimate()
    local expectedSum = 0
    for k = 1, 10 do
       expectedSum = expectedSum + ys[k]
-      local actual = Knn:estimate(xs, ys, query, k)
+      local knn = Knn
+      local actual = knn:estimate(xs, ys, query, k)
       local expected = expectedSum / k
       tester:asserteq(expected, actual, 'k=' .. k)
    end
 end
 
-function tests:testSmooth()
+-- test smoothing without using the query point
+function tests:testSmooth1()
    local nsamples, ndims, xs, ys = makeExample()
 
    local function smooth(queryIndex, k)
-      return Knn:smooth(xs, ys, queryIndex, k)
+      local knn = Knn()
+      local useQuery = false
+      return knn:smooth(xs, ys, queryIndex, k, useQuery)
    end
 
    tester:asserteq(2,   smooth(1,1), 'nearest neighbors = [2]')
@@ -48,9 +52,32 @@ function tests:testSmooth()
    tester:asserteq((2+4)/2,   smooth(3,2), 
                    'nearest neighbors = [2,4]')
    tester:asserteq((1+2+4+5)/4,   smooth(3,4), 
-                   'nearest neighbors = [1.2,4,5]')
+                   'nearest neighbors = [1,2,4,5]')
    tester:asserteq((55-3)/9, smooth(3,9), 
                    'nearest neighbors = [1,2,4,5,6,7,8,9,10]')
+
+end
+
+-- test smoothing using the query point
+function tests:testSmooth2()
+   local nsamples, ndims, xs, ys = makeExample()
+
+   local function smooth(queryIndex, k)
+      local knn = Knn()
+      local useQuery = true
+      return knn:smooth(xs, ys, queryIndex, k, useQuery)
+   end
+
+   tester:asserteq(1,   smooth(1,1), 'nearest neighbors = [2]')
+   tester:asserteq(1.5, smooth(1,2), 'nearest neighbors = [2,3]')
+   tester:asserteq(2,   smooth(1,3), 'nearest neighbors = [2,3,4]')
+
+   tester:asserteq((2+3+4)/3,   smooth(3,3), 
+                   'nearest neighbors = [2,3,4]')
+   tester:asserteq((1+2+3+4+5)/5,   smooth(3,5), 
+                   'nearest neighbors = [1,2,3,4,5]')
+   tester:asserteq((55-10)/9, smooth(3,9), 
+                   'nearest neighbors = [1,2,3,4,5,6,7,8,9]')
 
 end
 
